f7801701cb697be9121558fa45d7ae52167f948a
syr <blossomy1d@gmail.com>
2023-02-11 17:20:41 +0900

    역슬레시 수정

--- src/animal/Tiger.java
+++ src/animal/Tiger.java
@@ -2,7 +2,7 @@ package animal;
 
 //인터페이스 구현(implements)
 class Tiger extends Animal implements Predator, BarkAnimal { //콤마를 이용해 여러 인터페이스 구현 가능
-    public String getFood() { return "orange"; } //인터페이스의 메서드는 항상 public으로 구현
+    public String getFood() { return "orange"; } //인터페이스의 메서드는 항상 public 으로 구현
 
     public void bark() { System.out.println("어흥"); }
 }
