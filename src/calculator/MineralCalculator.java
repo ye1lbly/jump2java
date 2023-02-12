5eefc40434d4fc969df6c339a7c7400758bb27d4
syr <blossomy1d@gmail.com>
2023-02-12 16:26:17 +0900

    5장 연습문제 7번 - 중간값 생략하고 마지막 결과값만 나오게끔 코드 수정

--- src/calculator/MineralCalculator.java
+++ src/calculator/MineralCalculator.java
@@ -4,13 +4,16 @@ public class MineralCalculator {
     int value = 0;
 
     public void add(Mineral mineral) {
-        System.out.println(this.value += mineral.getValue());
+        this.value += mineral.getValue();
     }
+
+    public int getValue() { return this.value; }
 
     public static void main(String[] args) {
         MineralCalculator cal = new MineralCalculator();
         cal.add(new Gold());
         cal.add(new Sliver());
         cal.add(new Bronze());
+        System.out.println(cal.getValue());
     }
 }
