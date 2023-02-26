bc47e6c23b9b338121ef90c7b582359b80f4dd78
syr <blossomy1d@gmail.com>
2023-02-26 20:46:10 +0900

    3과 5의 배수 합 구하기

---
+++ src/practice/Practice4.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int n = 1; n < 1000; n++) {
+            if (n % 3 == 0 || n % 5 == 0) {
+                result += n;
+            }
+        }
+        System.out.println(result);
+    }
+}
