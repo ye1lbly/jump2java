9e789b76257fbe4da1d1f54ff29c00b23986e97a
syr <blossomy1d@idrsystems.co.kr>
2023-02-28 11:03:31 +0900

    9장 종합문제 6~8번 풀이 완료

--- src/practice/Practice5.java
+++ src/practice/Practice5.java
@@ -1,6 +1,7 @@
 package practice;
 
 import java.awt.*;
+import java.io.*;
 import java.util.*;
 
 public class Practice5 {
@@ -14,8 +15,8 @@ public class Practice5 {
         }
     }
 
-    public static void main(String[] args) {
-        //Q9-1.
+    public static void main(String[] args) throws IOException {
+        /* Q9-1 */
         String s = "a:b:c:d";
         System.out.println(s);
 
@@ -25,44 +26,81 @@ public class Practice5 {
         String result = String.join("#", data);
         System.out.println(result);
 
-        //Q9-2.
+        /* Q9-2 */
         HashMap<String, Integer> h = new HashMap<>();
         h.put("B", 80);
         System.out.println(h.getOrDefault("C", 70)); //h Map 에는 C 라는 key 가 없으므로 null 이 출력되겠지만, getOrDefault 를 통해 값을 얻을 수 있게 함
 
-        //Q9-3.
+        /* Q9-3 */
         int[] A = {20, 55, 67, 82, 45, 33, 90, 87, 100, 25};
         int a = Arrays.stream(A).filter((n) -> n >= 50).sum();
         System.out.println(a);
 
-        //Q9-4.
+        /* Q9-4 */
         for (int i = 0; i < 10; i++) { System.out.print(fibonnaci(i) + " "); }
 
-        //Q9-5.
+        /* Q9-5 */
         Scanner sc = new Scanner(System.in); //Scanner 클래스를 사용하기 위해 인스턴스 생성
         System.out.print("출력을 원하는 구구단 숫자를 입력하시오 : "); //print 를 사용하면 줄바꿈 없이 값 출력 (println 을 사용하면 줄바꿈된 출력값 확인 가능)
         int num = sc.nextInt(); //콘솔에서 숫자 입력 받음
         for (int p = 1; p < 10; p++) { System.out.printf("%d ", num * p); }
 
-        //Q9-6.
-        int[] user = {65, 45, 2, 3, 45, 8};
-        int u = Arrays.stream(user).sum();
-        System.out.println(u);
+        /* Q9-6 */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("총합을 구하고 싶은 숫자를 입력하시오 : ");
+        String userInput = scanner.nextLine(); //콘솔에서 한 줄 입력 받음
+        String[] numbers = userInput.split(",");
+        int total = 0;
+        for (String nums : numbers) {
+            nums = nums.trim(); //공백 제거
+            int N = Integer.parseInt(nums); //String -> Int
+            total += N;
+        }
+        System.out.printf("총합은 %d 입니다.\n", total);
 
-        //Q9-7.
-        String[] abc = {"AAA", "BBB", "CCC", "DDD", "EEE"};
-        ArrayList<String> alphabet = new ArrayList<>(Arrays.asList(abc));
+        /* Q9-7 */
+        ArrayList<String> alphabet = new ArrayList<>();
+
+        //파일의 각 라인을 읽고 alphabet 에 저장
+        BufferedReader br = new BufferedReader(new FileReader("abc.txt"));
+        while (true) {
+            String line = br.readLine();
+            if (line == null) break;
+            alphabet.add(line);
+        }
+        br.close();
+
         alphabet.sort(Comparator.reverseOrder());
-        System.out.println(alphabet);
 
-        //Q9-8.
-        double[] sample = {70, 60, 55, 75, 95, 90, 80, 80, 87, 100};
-        double sum = Arrays.stream(sample).sum();
-        System.out.println(sum);
+        //역순 정렬된 내용 result.txt 에 저장
+        PrintWriter pw = new PrintWriter("result.txt");
+        for (String line : alphabet) {
+            pw.println(line);
+        }
+        pw.close();
 
-        double avg = sum / sample.length;
-        System.out.println(avg); //소수점까지 표현해주기 위해 int 대신 double 로 수정
+        /* Q9-8 */
+        ArrayList<String> numbs = new ArrayList<>();
+        BufferedReader bdr = new BufferedReader(new FileReader("sample.txt"));
+        while (true) {
+            String line = bdr.readLine();
+            if (line == null) break;
+            numbs.add(line);
+        }
+        br.close();
 
+        int sum = 0;
+        for (String line : numbs) {
+            line = line.trim();
+            int N = Integer.parseInt(line);
+            sum += N;
+        }
 
+        double avg = (double) sum / numbs.size(); //소수점까지 표현해주기 위해 int 대신 double 로 수정
+        System.out.printf("총합 : %d, 평균 : %.2f\n", sum, avg); // %.2f 를 사용하여 소수점 이하 둘째 자리까지만 나타냄
+
+        PrintWriter ptw = new PrintWriter("result2.txt");
+        ptw.write(String.format("%.2f", avg)); //실수 문자열로 변환 후 저장
+        ptw.close();
     }
 }
